fac45211da13fa18d931fc77d533fb42766ebf84
Jarek Cymbalista <kalima12345@gmail.com>
2020-11-04 13:43:31 +0000

    busiest airport

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(function (req, res, next) {
   res.setTimeout(180000, function () {
     console.log("Request has timed out.");
